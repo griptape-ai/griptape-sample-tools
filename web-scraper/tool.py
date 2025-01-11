af0f8c92ec92d082e1020bc2e511eba992e5bf36
matt <matt@griptape.ai>
2025-01-11 16:41:02 -0600

    Add WebScraperTool Sample

---
+++ web-scraper/tool.py
@@ -0,0 +1,31 @@
+import os
+
+from griptape.tools import WebScraperTool
+from griptape.drivers import (
+    MarkdownifyWebScraperDriver,
+    ProxyWebScraperDriver,
+)
+
+
+def init_tool() -> WebScraperTool:
+    driver = MarkdownifyWebScraperDriver()
+    # Check the environment variable to determine which driver to use
+    if (zenrows_api_key := os.getenv("ZENROWS_API_KEY")) is not None:
+        # best effort default params
+        zenrows_params = [
+            "js_render=true",
+            "wait=5000",
+            "premium_proxy=true",
+            "markdown_response=true",
+        ]
+        proxy = (
+            f"http://{zenrows_api_key}:{'&'.join(zenrows_params)}@api.zenrows.com:8001"
+        )
+        proxies = {"http": proxy, "https": proxy}
+        params = {
+            "verify": False,
+            "timeout": 120,
+        }
+        driver = ProxyWebScraperDriver(proxies=proxies, params=params)
+
+    return WebScraperTool(web_scraper_driver=driver)
